ddbe31b0ed3d0ff882506752859c0336cfbcd5c2
Andrea <andrealmj@live.com>
2018-12-07 13:21:45 +0800

    added game pseudocode

---
+++ project-1/main.js
@@ -0,0 +1,24 @@
+When page is first loaded, cute cat image appears. The cat is holding / playing with her yarn. The yarn rolls away under one of the 3 red cups.
+
+"Help the Kitty find her ball of yarn!"
+
+Start button - on click, starts the game play
+
+OR
+
+"Help the Kitty find her ball of yarn!"
+
+Start button - on click, starts the game play, sad kitty (image) in the background. When yarn is found, happy kitty pops up over screen.
+
+--===GAME PLAY===--
+
+When "start" button is clicked,
+1. all cups are lifted up
+2. ball goes underneath one of the cups (random)
+3. all cups are lowered back to original position (ball cannot be seen anymore)
+4. cups are shuffled at a set speed
+5. when cups stop shuffling, user should click on the cup that they think holds the ball.
+6a. if the cup that is clicked DOES contain the ball, show happy kitty playing with ball.
+6b. if the cup that is clicked does NOT contain the ball, repeat from step 1.
+
+7. when ball is found, "play again?" button appears. repeat from step 1, except that this time the shuffling speed (as set in step 4) is increased.
